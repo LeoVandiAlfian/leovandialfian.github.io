32c790545caf33785cf02015262073363f01c6ee
LeoVandiAlfian <leovandialfian99@mgmail.com>
2022-01-13 19:26:09 +0700

    upload

---
+++ versi2/java.js
@@ -0,0 +1,7 @@
+let content1 = document.querySelector('.skill .content1');
+let content2 = document.querySelector('.skill .content2');
+
+content1.addEventListener('click', function(){
+    content1.classList.toggle('active');
+    content1.classList.toggle('actives');
+})
